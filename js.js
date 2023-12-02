d7b12b87f1e2f6e41b75a4570af6581d8468fe4e
Brian Riback <briback@labradorconnect.com>
2023-12-02 07:11:44 -0500

    Update js.js

--- js.js
+++ js.js
@@ -89,3 +89,46 @@ function addAnimation() {
     });
   });
 }
+
+// Get all links that start with #modal
+const modalLinks = document.querySelectorAll('a[href^="#modal"]');
+
+modalLinks.forEach(function (modalLink, index) {
+  // Get modal ID to match the modal
+  const modalId = modalLink.getAttribute('href');
+  
+  // Click on link
+  modalLink.addEventListener('click', function (event) {
+    
+    // Get modal element
+    const modal = document.querySelector(modalId);
+    // If modal with an ID exists
+    if(modal){
+      // Get close button
+      const closeBtn = modal.querySelector('.dialog__close');
+      event.preventDefault();
+      modal.showModal(); // Open modal
+      
+      // Close modal on click
+      closeBtn.addEventListener('click', function (event) {
+        modal.close();
+      });
+      
+      // Close modal when clicking outside modal
+      document.addEventListener('click', function (event) {
+        
+        const dialogEl = event.target.tagName;
+        const dialogElId = event.target.getAttribute('id');
+        if(dialogEl == 'DIALOG'){
+          // Close modal
+          modal.close();
+        }
+      }, false);
+      
+    // If modal ID not exists
+    } else {
+      console.log('Modal doesn\'t exist');
+    }
+  });
+});
+
